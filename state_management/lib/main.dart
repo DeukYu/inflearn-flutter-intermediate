108574b1ccf2f692841b4c8bd5462fc35b0931b3
DeukYu <ldy8070@naver.com>
2024-03-19 00:10:26 +0900

    provider observer

--- state_management/lib/main.dart
+++ state_management/lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:state_management/riverpod/provider_observer.dart';
 import 'package:state_management/screen/home_screen.dart';
 
 void main() {
   runApp(ProviderScope(
+    observers: [
+      Logger(),
+    ],
     child: MaterialApp(
       home: HomeScreen(),
     ),
