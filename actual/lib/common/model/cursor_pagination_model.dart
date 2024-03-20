42a7bcd971b6f65ec2a1ad9c90864b1b2a3dcd86
DeukYu <ldy8070@naver.com>
2024-03-20 22:50:06 +0900

    Cursor Pagination

--- actual/lib/common/model/cursor_pagination_model.dart
+++ actual/lib/common/model/cursor_pagination_model.dart
@@ -19,7 +19,20 @@ class CursorPagination<T> extends CursorPaginationBase {
   final CursorPaginationMeta meta;
   final List<T> data;
 
-  CursorPagination({required this.meta, required this.data});
+  CursorPagination({
+    required this.meta,
+    required this.data,
+  });
+
+  CursorPagination copyWith({
+    CursorPaginationMeta? meta,
+    List<T>? data,
+  }) {
+    return CursorPagination(
+      meta: meta ?? this.meta,
+      data: data ?? this.data,
+    );
+  }
 
   factory CursorPagination.fromJson(
           Map<String, dynamic> json, T Function(Object? json) fromJsonT) =>
@@ -31,7 +44,20 @@ class CursorPaginationMeta {
   final int count;
   final bool hasMore;
 
-  CursorPaginationMeta({required this.count, required this.hasMore});
+  CursorPaginationMeta({
+    required this.count,
+    required this.hasMore,
+  });
+
+  CursorPaginationMeta copyWith({
+    int? count,
+    bool? hasMore,
+  }) {
+    return CursorPaginationMeta(
+      count: count ?? this.count,
+      hasMore: hasMore ?? this.hasMore,
+    );
+  }
 
   factory CursorPaginationMeta.fromJson(Map<String, dynamic> json) =>
       _$CursorPaginationMetaFromJson(json);
