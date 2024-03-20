9ab76d8e465b2901ebd943d3902d7baee976e32e
DeukYu <ldy8070@naver.com>
2024-03-20 23:44:33 +0900

    skeletons 적용

--- actual/lib/restaurant/view/restraurant_detail_screen.dart
+++ actual/lib/restaurant/view/restraurant_detail_screen.dart
@@ -6,6 +6,7 @@ import 'package:actual/restaurant/model/restraurant_model.dart';
 import 'package:actual/restaurant/provider/restraurant_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:skeletons/skeletons.dart';
 
 class RestaurantDetailScreen extends ConsumerStatefulWidget {
   final String id;
@@ -44,10 +45,36 @@ class _RestaurantDetailScreenState
           renderTop(
             model: state,
           ),
+          if (state is! RestaurantDetailModel) renderLoading(),
           if (state is RestaurantDetailModel) renderLabel(),
           if (state is RestaurantDetailModel)
             renderProducts(products: state.products),
         ],
+      ),
+    );
+  }
+
+  SliverPadding renderLoading() {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(
+        horizontal: 16.0,
+        vertical: 16.0,
+      ),
+      sliver: SliverList(
+        delegate: SliverChildListDelegate(
+          List.generate(
+            3,
+            (index) => Padding(
+              padding: const EdgeInsets.only(bottom: 32.0),
+              child: SkeletonParagraph(
+                style: SkeletonParagraphStyle(
+                  lines: 5,
+                  padding: EdgeInsets.zero,
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
