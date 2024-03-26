b0e1bdc230a6e1ebffaf6f6440f0ca9188d29f6e
DeukYu <ldy8070@naver.com>
2024-03-27 00:52:43 +0900

    userRepository 생성

---
+++ actual/lib/user/repository/user_repository.dart
@@ -0,0 +1,26 @@
+import 'package:actual/common/const/data.dart';
+import 'package:actual/common/dio/dio.dart';
+import 'package:actual/user/model/user_model.dart';
+import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:retrofit/http.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'user_repository.g.dart';
+
+final userRepositoryProvider = Provider<UserRepository>((ref) {
+  final dio = ref.watch(dioProvider);
+
+  return UserRepository(dio, baseUrl: 'http://$ip/user/me');
+});
+
+@RestApi()
+abstract class UserRepository {
+  factory UserRepository(Dio dio, {String baseUrl}) = _UserRepository;
+
+  @GET('/')
+  @Headers({
+    'accssToken': 'true',
+  })
+  Future<UserModel> getMe();
+}
