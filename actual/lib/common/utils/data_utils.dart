a5e7e9abb566b529f523dcd1a5e39d6bc413339f
DeukYu <ldy8070@naver.com>
2024-03-27 21:37:43 +0900

    Login Token Refresh Repository

--- actual/lib/common/utils/data_utils.dart
+++ actual/lib/common/utils/data_utils.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:actual/common/const/data.dart';
 
 class DataUtils {
@@ -8,5 +10,13 @@ class DataUtils {
   static List<String> listpathsToUrls(List values) {
     if (values.isEmpty) return [];
     return values.map((e) => pathToUrl(e)).toList();
+  }
+
+  static String plainToBase64(String plain) {
+    Codec<String, String> stringToBase64 = utf8.fuse(base64);
+
+    String encoded = stringToBase64.encode(plain);
+
+    return encoded;
   }
 }
---
+++ actual/lib/user/repository/auth_repository.dart
@@ -0,0 +1,40 @@
+import 'package:actual/common/model/login_response.dart';
+import 'package:actual/common/model/token_response.dart';
+import 'package:actual/common/utils/data_utils.dart';
+import 'package:dio/dio.dart' hide Headers;
+
+class AuthRepository {
+  final Dio dio;
+  final String baseUrl;
+
+  AuthRepository({required this.dio, required this.baseUrl});
+
+  Future<LoginResponse> login({
+    required String username,
+    required String password,
+  }) async {
+    final serialized = DataUtils.plainToBase64('$username:$password');
+
+    final res = await dio.post(
+      '$baseUrl/login',
+      options: Options(
+        headers: {
+          'authorization': 'Basic $serialized',
+        },
+      ),
+    );
+
+    return LoginResponse.fromJson(res.data);
+  }
+
+  Future<TokenResponse> token() async {
+    final res = await dio.post(
+      '$baseUrl/token',
+      options: Options(
+        headers: {'refreshToken': 'true'},
+      ),
+    );
+
+    return TokenResponse.fromJson(res.data);
+  }
+}
