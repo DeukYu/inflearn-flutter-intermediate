fc0af8dd21bf2f5b0c2effff50113afc71629fc5
DeukYu <ldy8070@naver.com>
2024-04-02 23:55:52 +0900

    Create order_provider.dart

---
+++ actual/lib/order/provider/order_provider.dart
@@ -0,0 +1,53 @@
+import 'package:actual/order/model/order_model.dart';
+import 'package:actual/order/model/post_order_body.dart';
+import 'package:actual/order/repository/order_repository.dart';
+import 'package:actual/user/provider/basket_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:uuid/uuid.dart';
+
+final OrderProvider =
+    StateNotifierProvider<OrderStateNotifier, List<OrderModel>>((ref) {
+  final repo = ref.watch(orderRepositoryProvider);
+  return OrderStateNotifier(ref: ref, repository: repo);
+});
+
+class OrderStateNotifier extends StateNotifier<List<OrderModel>> {
+  final Ref ref;
+  final OrderRepository repository;
+
+  OrderStateNotifier({
+    required this.ref,
+    required this.repository,
+  }) : super([]);
+
+  Future<bool> postOrder() async {
+    try {
+      final uuid = Uuid();
+
+      final id = uuid.v4();
+
+      final state = ref.read(basketProvider);
+
+      await repository.postOrder(
+          body: PostOrderBody(
+        id: id,
+        totalPrice: state.fold<int>(
+          0,
+          (p, n) => p + (n.count * n.product.price),
+        ),
+        products: state
+            .map(
+              (e) => PostOrderBodyProduct(
+                productId: e.product.id,
+                count: e.count,
+              ),
+            )
+            .toList(),
+        createAt: DateTime.now().toString(),
+      ));
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+}
