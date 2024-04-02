0946307ec9a8ba104179c8fa6a86a077b5334039
DeukYu <ldy8070@naver.com>
2024-04-02 21:40:48 +0900

    장바구니 UI 작업

--- actual/lib/product/component/product_card.dart
+++ actual/lib/product/component/product_card.dart
@@ -110,13 +110,19 @@ class ProductCard extends ConsumerWidget {
           ),
         ),
         if (onSubtract != null && onAdd != null)
-          _Footer(
-            total: (basket.firstWhere((e) => e.product.id == id).count *
-                    basket.firstWhere((e) => e.product.id == id).product.price)
-                .toString(),
-            count: basket.firstWhere((e) => e.product.id == id).count,
-            onSubtract: onSubtract!,
-            onAdd: onAdd!,
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: _Footer(
+              total: (basket.firstWhere((e) => e.product.id == id).count *
+                      basket
+                          .firstWhere((e) => e.product.id == id)
+                          .product
+                          .price)
+                  .toString(),
+              count: basket.firstWhere((e) => e.product.id == id).count,
+              onSubtract: onSubtract!,
+              onAdd: onAdd!,
+            ),
           ),
       ],
     );
@@ -142,7 +148,7 @@ class _Footer extends StatelessWidget {
       children: [
         Expanded(
           child: Text(
-            "총액 $total",
+            "총액 ₩$total",
             style: const TextStyle(
               color: PRIMARY_COLOR,
               fontWeight: FontWeight.w500,
@@ -155,6 +161,7 @@ class _Footer extends StatelessWidget {
               icon: Icons.remove,
               onTap: onSubtract,
             ),
+            const SizedBox(width: 8.0),
             Text(
               count.toString(),
               style: const TextStyle(
@@ -162,6 +169,7 @@ class _Footer extends StatelessWidget {
                 fontWeight: FontWeight.w500,
               ),
             ),
+            const SizedBox(width: 8.0),
             renderButton(
               icon: Icons.add,
               onTap: onAdd,
--- actual/lib/restaurant/view/basket_screen.dart
+++ actual/lib/restaurant/view/basket_screen.dart
@@ -1,4 +1,8 @@
+import 'package:actual/common/const/colors.dart';
 import 'package:actual/common/layout/default_layout.dart';
+import 'package:actual/product/component/product_card.dart';
+import 'package:actual/user/provider/basket_provider.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -8,9 +12,118 @@ class BasketScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final basket = ref.watch(basketProvider);
+
+    if (basket.isEmpty) {
+      return DefaultLayout(
+          title: '장바구니',
+          child: Center(
+            child: Text(
+              '장바구니가 비어있습니다.',
+              textAlign: TextAlign.center,
+            ),
+          ));
+    }
+
+    final productsTotal = basket.fold<int>(
+      0,
+      (p, n) => p + (n.product.price * n.count),
+    );
+
+    final deliveryFee = basket.first.product.restaurant.deliveryFee;
+
     return DefaultLayout(
       title: '장바구니',
-      child: Column(),
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: ListView.separated(
+                  separatorBuilder: (context, index) {
+                    return Divider(
+                      height: 32.0,
+                    );
+                  },
+                  itemBuilder: (_, index) {
+                    final model = basket[index];
+                    return ProductCard.fromProductModel(
+                      model: model.product,
+                      onAdd: () {
+                        ref.read(basketProvider.notifier).addToBasket(
+                              product: model.product,
+                            );
+                      },
+                      onSubtract: () {
+                        ref
+                            .read(basketProvider.notifier)
+                            .removeFromBasket(product: model.product);
+                      },
+                    );
+                  },
+                  itemCount: basket.length,
+                ),
+              ),
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        '장바구니 금액',
+                        style: TextStyle(
+                          color: BODY_TEXT_COLOR,
+                        ),
+                      ),
+                      Text(
+                        '￦$productsTotal',
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '배달비',
+                        style: TextStyle(
+                          color: BODY_TEXT_COLOR,
+                        ),
+                      ),
+                      if (basket.length > 0)
+                        Text(
+                          '￦$deliveryFee',
+                        ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '총액',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Text('￦${deliveryFee + productsTotal}')
+                    ],
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: PRIMARY_COLOR),
+                        child: Text(
+                          '결제하기',
+                        )),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
